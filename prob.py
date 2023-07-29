7c3dbca45a35bea70c67508e8d2fa0aeb4dbe4e7
Giorgian Borca-Tasciuc <giorgian.borca-tasciuc@protonmail.com>
2023-07-29 10:58:03 -0400

    Began adding linear form estimator

--- prob.py
+++ prob.py
@@ -4,6 +4,87 @@ import numpy as np
 from itertools import chain
 from collections import defaultdict
 from abc import ABC, abstractmethod
+from sklearn.neighbors import KernelDensity
+
+class KProbabilityDensity:
+    """polynomially computes probability of input at given point"""
+    # TODO: add "given", which allows us to predicate on some variable when predicting the 
+    # continuous features
+    def __init__(self, X, input_class, n_generated_features=10, deg=5, bandwidth=0.1):
+        # TODO: check whether we still need this assumption, and if so, ensure it is enforced
+        # Assumption: One-Hot indices are contiguous in the array for a one-hot feature
+        matches_given = np.apply_along_axis(lambda x: x in input_class, 1, X)
+        if np.sum(matches_given) == 0:
+            raise ValueError("No instance of class found.")
+
+        X = X[matches_given]
+
+        self.discrete_indices = tuple(sorted(input_class.discrete_indices.values()))
+        self.continuous_indices = tuple(sorted(input_class.continuous_indices.values()))
+        self.one_hot_indices = tuple(sorted(input_class.one_hot_indices.values()))
+
+        self.discrete = [make_discrete_distribution(X[:, i]) for i in discrete_indices]
+        self.one_hot = [make_one_hot_distribution(X[:, index_group]) for index_group in one_hot_indices]
+
+        self.continuous_bounds = [(np.min(X[:, i]), np.max(X[:, i])) for i in continuous_indices]
+        self.discrete_bounds = [[(k, k) for k in sorted(d.keys())] for d in self.discrete]
+        self.one_hot_bounds = [[(one_hot(k, len(d.keys())), one_hot(k, len(d.keys()))) for k in sorted(d.keys())] for d in self.one_hot]
+
+
+        self.feature_generator = np.random.normal(size=(deg, n_generated_features, len(self.continuous_indices)))
+        self.bandwidth = bandwidth
+        self.coeffs, self.bias = self._fit(X)
+
+        
+        self.discrete_funcs = [make_discrete_func(d) for d in self.discrete]
+        self.one_hot_funcs = [make_one_hot_func(d) for d in self.one_hot]
+
+        self.continuous_volume = nquad(self._continuous_sample, self.continuous_bounds)
+        self.discrete_volumes = [sum(f(k) for k in d.keys()) for f, d in zip(self.discrete_funcs, self.discrete)]
+        self.one_hot_volumes = [sum(f(k) for k in d.keys()) for f, d in zip(self.one_hot_funcs, self.one_hot)]
+
+        regions = sorted(
+                chain(
+                    zip([[k] for k in continuous_indices], self.continuous_bounds),
+                    zip([[k] for k in discrete_indices], self.discrete_bounds),
+                    zip(one_hot_indices, self.one_hot_bounds)
+                    )
+
+        )
+        self._regions = tuple(map(lambda x: x[1], regions))
+
+    def _fit(X):
+        X_train = np.einsum('dfi,...i->...df', self.feature_generator, X[:, self.continuous_indices])
+        X_train = X_train.reshape(X_train.shape[0], -1)
+        X_test = np.random.uniform(size=(2*len(X), len(self.continuous_indices)))
+        kern = KernelDensity(kernel='epanechnikov', bandwidth=self.bandwidth).fit(X_train)
+        X_full = np.concatenate((X_train, X_test), axis=0)
+        y = np.exp(kern.score_samples(X_full))
+        lm = LinearRegression(positive=True).fit(X_full, y)
+        return lm.coef_.reshape(self.feature_generator.shape[:-1]), lm.intercept_
+
+    def _continuous_sample(self, x):
+        X = np.einsum('dfi,..i->...df', self.feature_generator, x)
+        y = np.einsum('...df,df->...', X, self.coeffs) + self.bias
+        return y
+
+    def sample(self, *args):
+        """get probability density at a point"""
+
+        x = np.array(args)
+        p = self._continuous_sample(x)/self.continuous_volume
+        for func, volume, index in zip(self.discrete_funcs, self.discrete_volumes, self.discrete_indices):
+            p *= func(x[index])/volume
+
+        for func, volume, index_group in zip(self.one_hot_funcs, self.one_hot_volumes, self.one_hot_indices):
+            p *= func(x[index_group])/volume
+
+        return p
+
+    @property
+    def regions(self):
+        return product(*self._regions)
+
 
 class ProbabilityDensityComputer:
     """computes probability of input at given point"""
@@ -82,6 +163,27 @@ class ProbabilityDensityComputer:
     def non_discretized_regions(self):
         return product(*self._non_discretized_regions)
 
+def make_poly_interpolation_func(pts, deg):
+    """converts a list of 2-d points to an interpolation function
+    assumes function is zero outside defined range
+    """
+
+
+    # assuming your points are stored in two lists: x_coords and y_coords
+    x_coords = np.array([x for x, y in pts])
+    y_coords = np.array([y for x, y in pts])
+
+    # fit the polynomial using the numpy Polynomial class
+    p = Polynomial.fit(x_coords, np.sqrt(y_coords), deg)
+
+    def f(x):
+        return p(x)**2
+
+
+    return f
+
+
+
 def make_linear_interpolation_func(pts):
     """converts a list of 2-d points to an interpolation function
     assumes function is zero outside defined range
@@ -137,6 +239,7 @@ def make_continuous_distribution(data):
     distribution = np.stack((centers, counts), axis=-1)
 
     return distribution
+
 
 def make_discrete_distribution(data):
     dist = defaultdict(int)
