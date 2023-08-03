f1acbedd9914f3e08601e7a921be13a722fed972
Giorgian Borca-Tasciuc <giorgian.borca-tasciuc@protonmail.com>
2023-08-03 11:35:49 -0400

    Added input class

--- input_spec.py
+++ input_spec.py
@@ -21,39 +21,10 @@ class Discrete:
 class OneHot:
     label: str 
 
-@dataclass(frozen=True)
-class InputClass:
-    boxes: Sequence[np.ndarray]
-
-    def __and__(self, other):
-        boxes = []
-        for box1, box2 in product(self.boxes, other.boxes):
-            intersection = np.concatenate((np.maximum(box1[:, 0], box2[:, 0])[:, None], np.minimum(box1[:, 1], box2[:, 1])[:, None]), axis=1)
-            if np.all(intersection[:, 0] <= intersection[:, 1]):
-                boxes.append(intersection)
-
-        return InputClass(boxes)
-
-    def __or__(self, other):
-        # TODO: Implement mutually exclusive regions. This will involve splitting the boxes
-        for i in range(len(self.boxes)):
-            for j in range(i + 1, len(other.boxes)):
-                intersection = np.concatenate((np.maximum(self.boxes[i][:, 0], other.boxes[j][:, 0])[:, None], np.minimum(self.boxes[i][:, 1], other.boxes[j][:, 1])[:, None]), axis=1)
-                if np.all(intersection[:, 0] <= intersection[:, 1]):
-                    warnings.warn("regions are not mutually exclusive")
-
-        return InputClass(tuple(self.boxes) + tuple(other.boxes))
-
-    def __not__(self):
-        raise NotImplementedError("Need to know the domain to implement this")
-
-    def __iter__(self):
-        return iter(self.boxes)
-
 class InputSpecification:
     def __init__(self, *variables):
         self.variables = {}
-        self.one_hot_variables = {}
+        self.one_hot_indices = {}
         self.discrete_indices = {}
         self.continuous_indices = {}
         self.n_variables = len(variables)
@@ -69,10 +40,10 @@ class InputSpecification:
                 self.variables[v.label] = v
                 self.discrete_indices[v.label] = i
             else:
-                if v.label not in self.one_hot_variables:
-                    self.one_hot_variables[v.label] = []
+                if v.label not in self.one_hot_indices:
+                    self.one_hot_indices[v.label] = []
 
-                self.one_hot_variables[v.label].append(i)
+                self.one_hot_indices[v.label].append(i)
 
     def get(self, **kwargs):
         box = np.zeros((self.n_variables, 2), dtype=np.float32)
@@ -82,7 +53,7 @@ class InputSpecification:
             elif isinstance(v, Discrete):
                 box[self.discrete_indices[k]] = [min(v.values), max(v.values)]
 
-        one_hot_specification = dict(self.one_hot_variables.items())
+        one_hot_specification = dict(self.one_hot_indices.items())
         for k, v in kwargs.items():
             # TODO: add checking for valid values
             if k in self.continuous_indices:
@@ -90,7 +61,7 @@ class InputSpecification:
             elif k in self.discrete_indices:
                 box[self.discrete_indices[k]] = v
             elif k in one_hot_specification:
-                one_hot_specification[k] = (self.one_hot_variables[k][v],)
+                one_hot_specification[k] = (self.one_hot_indices[k][v],)
             else:
                 raise ValueError(f'Invalid variable name {k}')
 
@@ -101,4 +72,41 @@ class InputSpecification:
                 box_copy[hot_values] = 1
             boxes.append(box_copy)
 
-        return InputClass(tuple(boxes))
+        return InputClass(self, tuple(boxes))
+
+@dataclass(frozen=True)
+class InputClass:
+    specification: InputSpecification
+    boxes: Sequence[np.ndarray]
+
+    def __and__(self, other):
+        boxes = []
+        for box1, box2 in product(self.boxes, other.boxes):
+            intersection = np.concatenate((np.maximum(box1[:, 0], box2[:, 0])[:, None], np.minimum(box1[:, 1], box2[:, 1])[:, None]), axis=1)
+            if np.all(intersection[:, 0] <= intersection[:, 1]):
+                boxes.append(intersection)
+
+        return InputClass(self.specification, boxes)
+
+    def __or__(self, other):
+        # TODO: Implement mutually exclusive regions. This will involve splitting the boxes
+        for i in range(len(self.boxes)):
+            for j in range(i + 1, len(other.boxes)):
+                intersection = np.concatenate((np.maximum(self.boxes[i][:, 0], other.boxes[j][:, 0])[:, None], np.minimum(self.boxes[i][:, 1], other.boxes[j][:, 1])[:, None]), axis=1)
+                if np.all(intersection[:, 0] <= intersection[:, 1]):
+                    warnings.warn("regions are not mutually exclusive")
+
+        return InputClass(self.specification, tuple(self.boxes) + tuple(other.boxes))
+
+    def __not__(self):
+        raise NotImplementedError("Need to know the domain to implement this")
+
+    def __iter__(self):
+        return iter(self.boxes)
+
+    def __contains__(self, X):
+        return all(self._in_box(X, box) for box in self.boxes)
+
+    @staticmethod
+    def _in_box(X, box):
+        return len(X) == len(box) and all(box[i, 0] <= X[i] <= box[i, 1] for i in range(len(X)))
